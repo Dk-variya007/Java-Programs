e96677179305caf0d10ce8c2fbcff8c65ef2d0c7
Dk-variya007 <variyadivyesh79@gmail.com>
2024-08-02 16:45:15 +0530

    "update code"

--- src/InterView_programs/pattern/pattern2.java
+++ src/InterView_programs/pattern/pattern2.java
@@ -1,10 +1,10 @@
 package InterView_programs.pattern;
 
-public class pattern1 {
+public class pattern2 {
     public static void pattern(int n) {
         int i, j;
         for (i = 1; i <= n; i++) {
-            for (j = 1; j <= i; j++) {
+            for (j = 1; j <= n-i; j++) {
                 System.out.print("* ");
             }
             System.out.println();
--- src/InterView_programs/pattern/pattern3.java
+++ src/InterView_programs/pattern/pattern3.java
@@ -1,17 +1,26 @@
 package InterView_programs.pattern;
 
-public class pattern1 {
+public class pattern3 {
     public static void pattern(int n) {
         int i, j;
         for (i = 1; i <= n; i++) {
+            for (j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
             for (j = 1; j <= i; j++) {
                 System.out.print("* ");
             }
             System.out.println();
         }
     }
+
     public static void main(String[] args) {
         int n = 5;
         pattern(n);
     }
 }
+//                       *
+//                      *  *
+//                     *  *  *
+//                    *  *  *  *
+//                  *  *  *  *   *
